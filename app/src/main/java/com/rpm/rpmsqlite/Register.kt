637ade3c8cbf511530c91cc0554a645f076b7371
Wilman <130717547+Wiilmar@users.noreply.github.com>
2024-02-14 15:32:01 -0500

    Nuevas funciones en layout

--- app/src/main/java/com/rpm/rpmsqlite/Register.kt
+++ app/src/main/java/com/rpm/rpmsqlite/Register.kt
@@ -1,8 +1,10 @@
 package com.rpm.rpmsqlite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import com.rpm.rpmsqlite.Model.ManagerDb
 import com.rpm.rpmsqlite.databinding.ActivityRegisterBinding
 
@@ -14,13 +16,29 @@ class Register : AppCompatActivity() {
         binding= ActivityRegisterBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
+
+
+        managerIU()
+    }
+    fun managerIU () {
+        listenerevents()
+    }
+
+    fun listenerevents() {
+        binding.BtnBack.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         binding.BtnRegister.setOnClickListener {
             val nombre= binding.ETName.text.toString()
             val apellido= binding.ETLastName.text.toString()
             val email= binding.ETEmail.text.toString()
             val password= binding.ETPassword.text.toString()
 
-           manager.insertUserData(nombre, apellido, email, password)
+            manager.insertUserData(nombre, apellido, email, password)
+            Toast.makeText(applicationContext, "Registro exitoso", Toast.LENGTH_SHORT).show()
         }
     }
 }
