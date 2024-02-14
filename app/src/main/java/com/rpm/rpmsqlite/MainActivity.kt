d09d07c586cf406877eba322e99f3fe948982f0c
unknown <martinesj405@gmail.com>
2024-02-14 15:14:51 -0600

    home desing fragments

--- app/src/main/java/com/rpm/rpmsqlite/MainActivity.kt
+++ app/src/main/java/com/rpm/rpmsqlite/MainActivity.kt
@@ -3,10 +3,16 @@ package com.rpm.rpmsqlite
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.rpm.rpmsqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(LayoutInflater.from(this))
@@ -15,24 +21,26 @@ class MainActivity : AppCompatActivity() {
 
 
         home()
+        initUi()
+
+    }
+
+    private fun initUi() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+
+        val navHost= supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as  NavHostFragment
+        navController= navHost.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+
 
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     private fun home() {
-        TODO("Not yet implemented")
+        Toast.makeText(this, "Welcome to RPM", Toast.LENGTH_SHORT).show()
+
     }
 }
