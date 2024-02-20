8241354ee31f0ce721e730c7d19faca9d6902cca
unknown <martinesj405@gmail.com>
2024-02-19 18:01:24 -0600

    bugfix

--- app/src/main/java/com/rpm/rpmsqlite/Model/Constains.kt
+++ app/src/main/java/com/rpm/rpmsqlite/Model/Constains.kt
@@ -4,8 +4,8 @@ class Constains {
 
     companion object{
         const val nomDb="rpmdb"
-        const val versionDb=1
-        const val USERS= "CREATE TABLE users(id int, name text , lastname text)"
+        const val versionDb=6
+        //const val USERS= "CREATE TABLE users(id int, name text , lastname text)"
         const val REGISTERMOTOS = "CREATE TABLE rmotos (marca text, modelo int, cilindraje int, placa text)"
         const val GETRMOTOS = "Select * from rmotos"
     }
